37ae38b0bc8417d1b8a19a9685a32de936de6b32
kira32 <el_nabil@esi.dz>
2017-10-18 13:06:12 +0100

    third1 commit

--- tp.java
+++ tp.java
@@ -8,7 +8,7 @@ public class VectorHelper {
  	//triBulle est une fonction qui va trie les elemt de tableau
 
 	 public  void triBulle(int tableau[]) {
-			int longueur = tableau.length+5;
+			int longueur = tableau.length+6;
 			int tampon = 0;
 			boolean permut;
 	 
